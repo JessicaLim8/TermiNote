4e4232304bcb6167257b213a6f575cf2faf88bf6
Jonathan Gin <jonathangin52@gmail.com>
2019-02-06 18:33:34 -0500

    Add version command, accessible via flag

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const minimist = require('minimist');
 const add = require('./commands/add');
 const list = require('./commands/list');
 const remove = require('./commands/remove');
+const version = require('./commands/version');
 
 module.exports = () => {
   const args = minimist(process.argv.slice(2));
-  const cmd = args._[0];
+  let cmd = args._[0];
+
+  // Added to support the more traditional method of passing flags
+  // Usage: note -v or note --version
+  if (args.v || args.version) {
+    cmd = 'version';
+  }
+
   switch (cmd) {
     case 'add':
       add(args);
@@ -15,6 +23,9 @@ module.exports = () => {
       break;
     case 'remove':
       remove(args);
+      break;
+    case 'version':
+      version();
       break;
     default:
       console.error(`"${cmd}" is not a valid command`);
