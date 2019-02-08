cfa21c2d2cda5f885d80a5e5b605ebd6f662a8c5
Jonathan Gin <jonathangin52@gmail.com>
2019-02-08 18:04:00 -0500

    Use OS to read homedir

--- commands/add.js
+++ commands/add.js
@@ -1,4 +1,19 @@
+const fs = require('fs');
+
 module.exports = (args) => {
-  console.log('You added a note!');
+  const dir = `${require('os').homedir()}/.terminote`;
+  const NOTES_FILE = 'notes.json';
+  const TO_DOS_FILE = 'to-dos.json';
+
+  // Check if ~/.terminote directory exists. If not, create the directory
+  try {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+  } catch (err) {
+    console.error('An error occured when attempting to create the ~/.terminote directory.');
+  }
+  // To add to the notes file, use the path `dir + NOTES_FILE`
+  // To add to the to-dos file, use the path `dir + TO_DOS_FILE`
 };
 
