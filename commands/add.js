59e3564bdc37d9be2792703f2da1af5941c61e5a
Jonathan Gin <jonathangin52@gmail.com>
2019-02-08 18:59:39 -0500

    Added fs.write

--- commands/add.js
+++ commands/add.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 module.exports = (args) => {
-  const dir = `${require('os').homedir()}/.terminote`;
+  const dir = `${require('os').homedir()}/.terminote/`;
   const NOTES_FILE = 'notes.json';
-  const TO_DOS_FILE = 'to-dos.json';
+  const TODOS_FILE = 'to-dos.json';
 
   // Check if ~/.terminote directory exists. If not, create the directory
   try {
@@ -13,7 +13,9 @@ module.exports = (args) => {
   } catch (err) {
     console.error('An error occured when attempting to create the ~/.terminote directory.');
   }
+  const CONTENT = 'hello world';
   // To add to the notes file, use the path `dir + NOTES_FILE`
-  // To add to the to-dos file, use the path `dir + TO_DOS_FILE`
+  // To add to the to-dos file, use the path `dir + TODOS_FILE`
+  fs.writeFile(dir + NOTES_FILE, CONTENT, (err) => err && console.error(err));
 };
 
