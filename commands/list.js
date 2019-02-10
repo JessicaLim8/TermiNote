dc086aedf0658e8db9b3433eea1187d1c432ee11
JessicaLim8 <jessicalim@bell.net>
2019-02-10 13:34:35 -0500

    Added ability to add list notes

--- commands/list.js
+++ commands/list.js
@@ -1,4 +1,25 @@
+const dir = `${require('os').homedir()}/.terminote/`;
+const color = require('colors');
+const error = require('../util/error');
+
 module.exports = (args) => {
-  console.log('Here are your notes!');
+  const file = require(dir + args.f);
+  const { title, titleColour, colour, type, entries } = file;
+  let checkbox;
+  console.log(title[titleColour] + '\n');
+  if (entries.length === 0) {
+    error('There are no entries in this list', true, 0);
+  }
+  for (let i = 0; i < entries.length; i++) {
+    if (type == 'Dot-jots') {
+      checkbox = ' ●';
+    }
+    else if (entries[i].checked === true) {
+      checkbox = '✅';
+    } else {
+      checkbox = '❌';
+    }
+    console.log(checkbox + '   ' + entries[i].content[colour]);
+  }
 };
 
