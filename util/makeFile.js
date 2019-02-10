4b4b376c0b2e72c6ef153afbb854ca89367a05bd
JessicaLim8 <jessicalim@bell.net>
2019-02-09 22:36:54 -0500

    added function that creates a file if it doesn't exist yet

---
+++ util/makeFile.js
@@ -0,0 +1,41 @@
+const dir = `${require('os').homedir()}/.terminote/`;
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+module.exports = (args) => {
+  let filename;
+  const questions = [{
+      type: 'list',
+      message: 'What kind of notes would you like this file to contain?',
+      name: 'type',
+      choices: ['Checkboxes', 'Dot-jots'],
+    },
+    {
+      type: 'list',
+      message: 'What is the default colour of this list',
+      name: 'colour',
+      choices: ['white', 'black', 'cyan', 'green', 'blue'],
+    }];
+  if (!args.f && !args.file) {
+    questions.unshift({
+      type: 'input',
+      message: 'What would you like to call your new file?',
+      name: 'name',
+    });
+  } else {
+    filename = args.f || args.file;
+  }
+  console.log(filename);
+  inquirer.prompt(questions).then((settings) => {
+    if (!filename) {
+      filename = settings.name;
+      delete settings.name;
+    }
+    if (!filename.includes('.json')) {
+      filename += '.json';
+    }
+    settings.entries = [];
+    fs.writeFile((dir + filename), JSON.stringify(settings), (err) => err && console.error(err));
+  }).catch((err) => console.error(err));
+};
+
