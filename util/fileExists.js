bd2e2f223f98ac517f7b7a680cdbbd8e51ba8de6
JessicaLim8 <jessicalim@bell.net>
2019-02-09 21:23:01 -0500

    changed fileExist to catch filenames without extensions

--- util/fileExists.js
+++ util/fileExists.js
@@ -1,5 +1,10 @@
 const dir = `${require('os').homedir()}/.terminote/`;
 const fs = require('fs');
 
-module.exports = (filename) => fs.existsSync(dir + filename);
+module.exports = (filename) => {
+  if (!filename.includes('.json')) {
+    filename += '.json';
+  }
+  return fs.existsSync(dir + filename);
+}
 
