508d5ad0f150814374b614f3271338d488440d74
JessicaLim8 <jessicalim@bell.net>
2019-02-10 19:30:23 -0500

    changed formatting issues

--- commands/list.js
+++ commands/list.js
@@ -1,20 +1,18 @@
-const dir = `${require('os').homedir()}/.terminote/`;
 const color = require('colors');
 const error = require('../util/error');
 
 module.exports = (args) => {
-  const file = require(dir + args.f);
-  const { title, titleColour, colour, type, entries } = file;
+  const { title, titleColour, colour, type, entries } = require(`${require('os').homedir()}/.terminote/` + args.f);
   let checkbox;
   console.log(title[titleColour] + '\n');
 
-  if (entries.length === 0) {
+  if (!entries.length) {
     error('There are no entries in this list', true, 0);
   }
   for (let i = 0; i < entries.length; i++) {
     if (type === 'Dot-jots') {
       checkbox = ' ●';
-    } else if (entries[i].checked === true) {
+    } else if (entries[i].checked) {
       checkbox = '✅';
     } else {
       checkbox = '❌';
