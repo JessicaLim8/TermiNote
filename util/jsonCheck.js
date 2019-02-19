aa5291ed4df56df4aed95dbcabbc1c224b5765ed
JessicaLim8 <jessicalim@bell.net>
2019-02-18 23:49:34 -0500

    clarified json.check

--- util/jsonCheck.js
+++ util/jsonCheck.js
@@ -1,10 +1,6 @@
 module.exports = (filename) => {
-  // Returns if filename is invalid
-  if (!filename) {
-    return;
-  }
   // Adds a .json if the file deosnt have a .json
-  if (!filename.includes('.json')) {
+  if (filename && !filename.includes('.json')) {
     return filename + '.json';
   }
   return filename;
